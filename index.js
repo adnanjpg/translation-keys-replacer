5ea82976a45c1410dd775bfd4a5323bb1feb6627
adnanjpg <adnanfahedhr@gmail.com>
2023-11-12 13:10:49 +0300

    Add code to read and store all keys from JSON file.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ require('dotenv').config();
 const PROJECT_FILES_PATH = process.env.PROJECT_FILES_PATH;
 const JSON_FILE_PATH = process.env.JSON_FILE_PATH;
 
+// i wanna read all the keys in the json file and store them in an array
+const fs = require('fs');
+
+const jsonFile = fs.readFileSync(JSON_FILE_PATH, 'utf8');
+const jsonFileParsed = JSON.parse(jsonFile);
+const jsonFileKeys = Object.keys(jsonFileParsed);
+
+console.log(jsonFileKeys);
