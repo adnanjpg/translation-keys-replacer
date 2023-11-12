2a9e673c634c9e0a118f77ab836edeec5e9de8ad
adnanjpg <adnanfahedhr@gmail.com>
2023-11-12 13:41:43 +0300

    Add support for replacing getStr function calls with LocaleKeys and add import statement for generated keys file

---
+++ replace-get-str.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+const PROJECT_FILES_PATH = process.env.PROJECT_FILES_PATH;
+const GENERATED_KEYS_IMPORT_STATEMENT = process.env.GENERATED_KEYS_IMPORT_STATEMENT;
+
+// I wanna find all occurences of the calling of the function
+// 'getStr' or in the project, and do like the following:
+// getStr('key') -> getStr(LocaleKeys.key)
+// and add the value of GENERATED_KEYS_FILE_PATH to the top of the file
+
+const fs = require('fs');
+
+// loop the project files:
+// for each file, read it and replace all the keys with the new keys
+// then write the file back
+const recursive = require('recursive-readdir');
+
+const replaceForFile = (file) => {
+    let fileContent = fs.readFileSync(file, 'utf8');
+
+    let prevFileContent = fileContent;
+
+    fileContent = fileContent.replace(/getStr\('(.*)'\)/g, 'getStr(LocaleKeys.$1)');
+
+    // if the content changed, add the import
+    if (prevFileContent !== fileContent) {
+        fileContent = `${GENERATED_KEYS_IMPORT_STATEMENT}\n` + fileContent;
+    }
+
+    fs.writeFileSync(file, fileContent);
+}
+
+recursive(PROJECT_FILES_PATH, function (err, files) {
+    // Files is an array of filename
+    console.log(files);
+    files.forEach(replaceForFile);
+});
